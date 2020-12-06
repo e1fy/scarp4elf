9084ff9d59b1335c20f99cf2a12851ad82a7c69e
elf <61194758+e1fy@users.noreply.github.com>
2020-12-06 09:59:15 +0000

    Update scarp4elf.py

--- scarp4elf.py
+++ scarp4elf.py
@@ -1,42 +1,59 @@
 import requests
 import re
+import os
+alphacoders_regex_recon = r"center title'>(.*)<\/h1>"
+alphacoders_regex_recon_pages = r"<li><a.*page=(\d+).*<\/a><\/li>"
+alphacoders_regex_images = r"boxgrid.>\n*.*a href=.(.*).title"
+alphacoders_regex_get_image_url = r"main-content.*(https.*)."
+alphacoders_regex_name = r"https:.*\/(.+)"
 headers = {"User-Agent":"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.66 Safari/537.36"}
+def download_single_page(url):
+    request = requests.get(url,headers=headers)
+    source_code = request.text
+    regex_resault_images_match = re.findall(alphacoders_regex_images,source_code)
+    for ids in regex_resault_images_match:
+        get_by_id(ids)
 def scarp_alpha_coders(key):
-    #regexes and setup
     if(" " in key):
         key = key.replace(" ","+")
     url = f"https://wall.alphacoders.com/search.php?search={key}"
-    regex_recon = r"center title'>(.*)<\/h1>"
-    regex_recon_pages = r"<li><a.*page=(\d+).*<\/a><\/li>"
-    regex_images = r"boxgrid.>\n*.*a href=.(.*).title"
-    regex_get_image_url = r"main-content.*(https.*)."
-    regex_name = r"https:.*\/(.+)"
-    #recon
     recon = requests.get(url,headers=headers)
     recon_source = recon.text
     recon_source = recon_source.replace("\n","")
-    #results
-    recon_resault_images = re.findall(regex_recon,recon_source)
-    recon_resault_pages = re.findall(regex_recon_pages,recon_source)
+    recon_resault_images = re.findall(alphacoders_regex_recon,recon_source)
+    recon_resault_pages = re.findall(alphacoders_regex_recon_pages,recon_source)
     recon_resault_pages = list(dict.fromkeys(recon_resault_pages))
-    pages = recon_resault_pages[0]
-    print(f"{recon_resault_images[0]} {pages} pages - alphacoders")
-    #scarping
-    for i in range(1,int(pages)+1):
-        request = requests.get(f"https://wall.alphacoders.com/search.php?search={key}&page={str(i)}",headers=headers)
-        source_code = request.text
-        regex_resault_images_match = re.findall(regex_images,source_code)
-        for ids in regex_resault_images_match:
-            get_id_page = requests.get(f"https://wall.alphacoders.com/{ids}",headers=headers)
-            source_code_id_page = get_id_page.text
-            match_image = re.findall(regex_get_image_url,source_code_id_page)
-            name = match_image[0]
-            file_name = re.findall(regex_name,name)
-            with open(file_name[0],"wb") as f:
-                f.write(requests.get(name,headers=headers).content)
-            print(f"[+] downloaded - {name}")
+    if(len(recon_resault_pages)==0):
+        download_single_page(url)
+    else:
+        pages = recon_resault_pages[0]
+        print(f"{recon_resault_images[0]} {pages} pages - alphacoders")
+        for i in range(1,int(pages)+1):
+            request = requests.get(f"https://wall.alphacoders.com/search.php?search={key}&page={str(i)}",headers=headers)
+            source_code = request.text
+            regex_resault_images_match = re.findall(alphacoders_regex_images,source_code)
+            for ids in regex_resault_images_match:
+                get_by_id(ids)
+    return 0
+def get_by_id(ids):
+    get_id_page = requests.get(f"https://wall.alphacoders.com/{ids}",headers=headers)
+    source_code_id_page = get_id_page.text
+    match_image = re.findall(alphacoders_regex_get_image_url,source_code_id_page)
+    name = match_image[0]
+    file_name = re.findall(alphacoders_regex_name,name)
+    download(file_name[0],name)
+def download(file_name,file_url):
+    try:
+        with open(file_name,"wb") as f:
+            f.write(requests.get(file_url,headers=headers).content)
+            print(f"[+] downloaded - {file_name}")
+    except KeyboardInterrupt:
+            os.remove(file_name)
+            print("[!]keyboard interrupt")
+            os.close()
 def main():
-    keyword = "owari no seraph" #character goes here
+    keyword = "arch linux"
     scarp_alpha_coders(keyword)
+    print("done")
 if __name__ == "__main__":
     main()
