009ebbac4bd3eb49419f40a2a74843ebbba829f8
elf <61194758+e1fy@users.noreply.github.com>
2020-12-13 19:57:50 +0000

    Update scarp4elf.py

--- scarp4elf.py
+++ scarp4elf.py
@@ -1,4 +1,7 @@
+from sys import argv as arguments
+from sys import exit
 import requests
+import time
 import re
 import os
 alphacoders_regex_recon = r"center title'>(.*)<\/h1>"
@@ -32,6 +35,7 @@ def scarp_alpha_coders(key):
             download_single_page(f"https://wall.alphacoders.com/search.php?search={key}&page={str(i)}")
     return 0
 def get_by_id(ids):
+    time.sleep(0.5)
     get_id_page = requests.get(f"https://wall.alphacoders.com/{ids}",headers=headers)
     source_code_id_page = get_id_page.text
     match_image = re.findall(alphacoders_regex_get_image_url,source_code_id_page)
@@ -48,8 +52,10 @@ def download(file_name,file_url):
             print("[!]keyboard interrupt")
             os.close()
 def main():
-    keyword = "zero two"
+    if(len(arguments)==1):
+        exit()
+    else:
+        keyword = " ".join(arguments[1:])
     scarp_alpha_coders(keyword)
-    print("done")
 if __name__ == "__main__":
     main()
